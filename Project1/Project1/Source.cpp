41d50905dd6f74b880a8390d814ed9912129a457
Michael Lynn <hailviral101@yahoo.com>
2019-10-21 04:22:24 -0600

    clashofcode
    clashofcode

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,39 @@
+// CoC.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+		//*****************************************************************************************************
+		/*Construct a square using '+' characters with width and height as 'n'.*/
+		//*****************************************************************************************************
+
+
+		int s;
+		cin >> s; cin.ignore();
+		for (int i = 0; i < s; i++)
+		{
+			for (int j = 0; j < s; j++)
+			{
+				cout << "+";
+			}
+			cout << endl;
+		}
+
+
+		
+
+		//*****************************************************************************************************
+		/*Construct a square using '+' characters with width and height as 'n'.*/
+		//*****************************************************************************************************
+
+
+
+}
+
